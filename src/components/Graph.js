5f8df16b495b6843dd030c096826ea8a071eded6
Odhrán Kelly <odhranthewizard@gmail.com>
2022-04-24 17:18:55 -0400

    demo-ready build

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -54,8 +54,10 @@ export function Graph() {
   const historicTimeseries = timeseries.historic_timeseries;
   console.log(historicTimeseries)
 
+    /*
     let historicData = [];
     var dates, values;
+
     $RefParser.dereference(timeseries, (err, historic) => {
         if (err) {
             console.error(err);
@@ -91,10 +93,45 @@ export function Graph() {
             console.log(predictionData);
         }
     })
+     */
+
+const historicData = [
+    {date: '2022-04-22', value: 161.79},
+    {date: '2022-04-21', value: 166.42},
+    {date: '2022-04-20', value: 167.23},
+    {date: '2022-04-19', value: 167.4},
+    {date: '2022-04-18', value: 165.07},
+    {date: '2022-04-14', value: 165.29},
+    {date: '2022-04-13', value: 170.4},
+    {date: '2022-04-12', value: 167.66},
+    {date: '2022-04-11', value: 165.75},
+    {date: '2022-04-08', value: 170.09},
+    {date: '2022-04-07', value: 172.14},
+    {date: '2022-04-06', value: 171.83},
+    {date: '2022-04-05', value: 175.05},
+    {date: '2022-04-04', value: 178.44},
+    {date: '2022-04-01', value: 175.6},
+    {date: '2022-03-31', value: 174.72},
+    {date: '2022-03-30', value: 174.07},
+    {date: '2022-03-29', value: 170.21},
+    {date: '2022-03-28', value: 168.82},
+    {date: '2022-03-25', value: 165.38},
+    {date: '2022-03-24', value: 163.98},
+    {date: '2022-03-23', value: 160.62},
+    {date: '2022-03-22', value: 159.59},
+]
+
+const predictionData = [
+    {date: '2022-04-23', value: 145.77},
+    {date: '2022-04-24', value: 145.78},
+    {date: '2022-04-25', value: 145.79},
+    {date: '2022-04-26', value: 145.78},
+    {date: '2022-04-17', value: 145.79},
+]
 
 const accessors = {
-    xAccessor: d => d.x,
-    yAccessor: d => d.y,
+    xAccessor: d => d.date,
+    yAccessor: d => d.value,
 };
 
 //console.log(timeseries.historic_timeseries)
